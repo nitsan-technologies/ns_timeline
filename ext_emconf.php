9c7478e2514cc359eb7c0cf8169bb8c4bd694671
Pradeepsinh Masani <pradeep.nitsan@gmail.com>
2021-06-18 16:56:55 +0530

    [TODO] new tag release

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-	'title' => '[NITSAN] Nitsan Timeline',
-	'description' => 'Its Timeline Extension with Different Types of Variations',
+	'title' => '[NITSAN] Events & Timeline TYPO3 Extension',
+	'description' => 'All In One Timeline TYPO3 Extension lets you create stories, timelines, history, roadmap, and what not with unlimited possibilities and 18 unique styles and layouts.',
 	'category' => 'plugin',
-	'author' => 'NITSAN Technologies Pvt Ltd',
+	'author' => 'T3:Pradeepsinh Masani, T3:Keval Pandya, QA:Vandna Makwana',
 	'author_email' => 'info@nitsan.in',
 	'author_company' => 'NITSAN Technologies Pvt Ltd',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.2.0',
+	'version' => '1.2.1',
 	'constraints' => [
 		'depends' => [
 			'typo3' => '8.0.0-10.9.99',
