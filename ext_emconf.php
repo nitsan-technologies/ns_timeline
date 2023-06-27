e2f927b513ad2a058172b608ebc122df9ea031bc
NITSAN Technologies <sanjay@nitsan.in>
2023-06-27 16:02:38 +0530

    [TASK] Ready to release minor v12.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['ns_timeline'] = [
 	'title' => '[NITSAN] Events & Timeline TYPO3 Extension',
 	'description' => 'All In One Timeline TYPO3 Extension lets you create stories, timelines, history, roadmap, and what not with unlimited possibilities and 18 unique styles and layouts. Live-Demo: https://demo.t3planet.com//t3t-extensions/timeline You can download PRO version for more-features & free-support at https://t3planet.com/ns-timeline-typo3-extension',
 	'category' => 'plugin',
