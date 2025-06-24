e964e1dc61d927b07856881b094f6ee91d5c2566
NITSAN <info@nitsan.in>
2025-06-24 17:18:57 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,13 @@
 <?php
 
 $EM_CONF['ns_timeline'] = [
-    'title' => 'Events & Timeline',
-    'description' => 'Create engaging stories, timelines, histories, and roadmaps easily with the All In One Timeline TYPO3 Extension. With 18 unique styles and layouts, this extension offers unlimited possibilities for showcasing content on your TYPO3 website. Additionally, the TYPO3 Event extension integrates with TYPO3 Blog and TYPO3 News extensions allowing you to display timeline posts.
+    'title' => 'TYPO3 Timeline & Events Extension',
+    'description' => 'Create interactive stories, visual roadmaps, and chronological events with 18+ unique timeline styles. Perfect for company history, product roadmaps, or storytelling in your TYPO3 site. Supports integration with TYPO3 Blog and News extensions.',
 
-    *** Live Demo: https://demo.t3planet.com/t3-extensions/timeline *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-timeline-extension',
     'category' => 'plugin',
-    'author' => 'T3: Vivek Solanki, QA: Krishna Dhapa',
-    'author_email' => 'info@nitsan.in',
-    'author_company' => 'T3Planet // NITSAN',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
